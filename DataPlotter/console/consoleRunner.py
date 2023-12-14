71684cef1abcd6fa235f41c6fcc1c4fe5bff4aa0
Matei-Bogdan Plescan <matei.plescan@stud.acs.upb.ro>
2023-12-14 23:24:45 +0200

    Added comments for clarity

--- DataPlotter/console/consoleRunner.py
+++ DataPlotter/console/consoleRunner.py
@@ -13,6 +13,19 @@ import console.popUpManager as popUp
 # - display the polynomial function
 # - display the extrapolated point
 # - trigger the calculation of the integral of the polynomial function
+#
+# Needed buttons:
+# - add/remove points (select between add point, add multiple points from file, remove point)
+# - calculate integral/derivative (select between them - change the mode of the api to line)
+# - linreg (change the mode of the api to line)
+# - see next predicted point (change the mode of the api to dtm)
+# - extrapolate point (change the mode of the api to line)
+#
+# Data displaying:
+# - when mode is none, display the points
+# - when mode is line, display the points and the polynomial function
+# - when mode is dtm, display the points
+
 def runner():
     layout = [[psg.Text(text='Hello World',
                         font=('Arial Bold', 20),
