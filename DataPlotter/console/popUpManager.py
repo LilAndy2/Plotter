94eb904b758c5a1bfdd1a05bf15fcf5a8ecb06e8
dariuusss <119854575+dariuusss@users.noreply.github.com>
2023-12-16 18:06:37 +0200

    pretty graph now

--- DataPlotter/console/popUpManager.py
+++ DataPlotter/console/popUpManager.py
@@ -2,6 +2,7 @@ import PySimpleGUI as psg
 from data_processing.point import Point
 from data_processing.processorAPI import ProcessorAPI
 from matplotlib import pyplot as plt
+import os
 
 
 def welcomePopUp():
@@ -9,9 +10,19 @@ def welcomePopUp():
     text = file.read()
     psg.popup_scrolled(text, title="Scrolled Popup", font=("Arial Bold", 16), size=(70, 10))
 
+puncte = []
+
+# Numele fișierului PNG
+nume_fisier_png = 'grafic.png'
+
+# Variabilă pentru a ține evidența dacă legenda a fost deja adăugată
+legenda_adaugata = False
 
 def updatePlot():
+    global legenda_adaugata
     fig, ax = plt.subplots()
+
+    # Adăugare toate punctele existente
     for p in puncte:
         ax.scatter(p.x, p.y, color='red', marker='o', label='Punct')
 
@@ -23,18 +34,20 @@ def updatePlot():
     # Adăugare rețea pe axă
     ax.grid(True)
 
-    # Adăugare legendă
-    ax.legend()
+    # Salvare și actualizare imagine PNG
+    plt.savefig(nume_fisier_png)
+    plt.draw()
 
-    # Salvare imagine PNG
-    plt.savefig('grafic.png')
+    # Adăugare legenda (doar dacă nu a fost deja adăugată)
+    if not legenda_adaugata:
+        ax.legend()
+        legenda_adaugata = True
 
     # Afișare grafic în fereastră
     plt.show()
 
-puncte = []
-
 def addPointPopUp():
+    global puncte  # Declară variabila puncte ca fiind globală pentru a o putea modifica în funcție
     psg.set_options(font=('Arial Bold', 16))
     layout = [
         [psg.Text('X ', size=(10, 1)), psg.Input(expand_x=True)],
@@ -56,6 +69,7 @@ def addPointPopUp():
         return point
 
     return None
+
 
 def integralPopUp(processor):
     psg.set_options(font=('Arial Bold', 16))
